83928a2b6e99ed29fdbad728512f9e39317ce2c3
your email address <your email address>
2022-11-19 14:46:05 +0200

    almost complete

---
+++ js/code.js
@@ -0,0 +1,67 @@
+let btnChar = document.querySelector('#btnChar');
+let btnSearch = document.querySelector('#btnSearch');
+
+// Implementation
+// Characters button
+btnChar.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let enterSentence = document.querySelector('#enterSentence').value;
+    console.log(enterSentence.length);
+    let numChar = document.querySelector('.numChar').innerHTML;
+    document.querySelector('.numChar').innerHTML =  numChar.replace('??', enterSentence.length);
+
+})
+//Search button
+btnSearch.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let enterSentence = document.querySelector('#enterSentence').value;
+    let enterWord = document.querySelector('#enterWord').value;
+    let extra = (`${enterSentence.indexOf(enterWord)}`);
+    let valuePosition = document.querySelector('.valuePosition').innerHTML;
+    document.querySelector('.valuePosition').innerHTML = valuePosition.replace('??', `${extra}`);
+})
+
+
+
+
+
+
+
+
+
+
+// console.log(`indexOf(): ${sentence.indexOf('love')}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btnSearch.addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     information.push(enterSentence.value);
+//     enterSentence.value = "";
+//     let enterWord = document.querySelector('#enterWord');
+//     information.push(enterWord.value);
+//     enterWord.value = "";
+// }); 
+
+//Char Button 
+// btnChar.addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     let numChar = document.querySelector('.numChar').innerHTML;
+//     let enterSentence = document.querySelector('#enterSentence');
+//     let charResults = (enterSentence.length);
+//     numChar.replace('/?/g',`${charResults}`);
+
+// });
+
+
